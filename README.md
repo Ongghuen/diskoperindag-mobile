febd8a0f8b3b9ca88c6d55a6f52e9019dd0820eb
Raihan <17radf@gmail.com>
2023-03-24 16:55:11 +0700

    feat: add initial barebones and readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Android
+Diskoperindag - News and Forms Centre App
+Todo
+
+#### Install
+Just sync gradle and you're done. nyalain api di [web](https://github.com/ongghuen/diskoperindag-web). 
+
+#### Todos
+- [ ] Login
+- [ ] News
+- [ ] Apps
--- app/src/main/java/com/example/diskoperindag/MainActivity.kt
+++ app/src/main/java/com/example/diskoperindag/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.diskoperindag
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin = findViewById(R.id.btn_login)
+        btnLogin.setOnClickListener {
+            login()
+        }
     }
+
+    private fun login() {
+        val intent = Intent(this, DashboardActivity::class.java)
+        startActivity(intent)
+    }
+
 }
